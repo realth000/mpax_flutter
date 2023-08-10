46d21cdb9607fa1a4b7be2f428a6c794504755b5
realth000 <a598919335@gmail.com>
2023-08-11 00:37:26 +0800

    chore(*): Remove legacy code

--- lib/.legacy/desktop/pages/playlist_page/playlist_page_controller.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-
-import 'package:mpax_flutter/models/playlist_model.dart';
-
-/// Controller of playlist page.
-class DesktopPlaylistPageController extends GetxController {
-  /// Current showing playlist.
-  final currentPlaylist = Playlist().obs;
-}
