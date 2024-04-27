f6a0b79579bdab1895a7333eaed6aeed90f5fac8
realth000 <a598919335@gmail.com>
2024-04-27 17:59:27 +0800

    fix(test): Fix ci test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,8 +5,8 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-
+import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  //
+  test('empty', () {});
 }
